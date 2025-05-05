7696bb88cb3539a79405be01f1b2dbc7291fba24
cvazy <player7712@mail.ru>
2025-05-05 19:06:24 +0300

    Back init

--- delivery_project/settings.py
+++ delivery_project/settings.py
@@ -172,7 +172,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://10.0.2.2:8000',
-    'https://delivery-front.netlify.app/'
+    'https://delivery-front.netlify.app'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
